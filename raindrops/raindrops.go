330a5974e461c576c4b491553cc0c4472d806f28
WGong <wen.g.gong@gmail.com>
2018-12-22 19:33:19 -0500

    scrabble-score

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,38 +1,22 @@
-// Package raindrops translates key number(s) to phrase
+// Package raindrops translates factor to phrase
 package raindrops
 
-import (
-	"fmt"
-	"math"
-)
+import "strconv"
 
-// use map to store factors
-func factors(N int) map[int]int {
-	mFact := map[int]int{1: 1, N: 1}
-	for i := 2; i < int(math.Sqrt(float64(N)))+1; i++ {
-		if N%i == 0 {
-			mFact[i] = 1
-			mFact[int(N/i)] = 1
-		}
-	}
-	return mFact
-}
-
-// Convert translates number to phrase
+// Convert checks if given number has factor (3,5,7) then output phrase
 func Convert(N int) string {
-	mFactors := factors(N)
-	lNumbers := []int{3, 5, 7}
-	mWords := map[int]string{3: "Pling", 5: "Plang", 7: "Plong"}
-
 	phrase := ""
-	for _, k := range lNumbers {
-		if _, ok := mFactors[k]; ok {
-			phrase += mWords[k]
-		}
+	if N%3 == 0 {
+		phrase += "Pling"
+	}
+	if N%5 == 0 {
+		phrase += "Plang"
+	}
+	if N%7 == 0 {
+		phrase += "Plong"
 	}
 	if phrase == "" {
-		phrase = fmt.Sprintf("%d", N)
+		phrase = strconv.Itoa(N)
 	}
-
 	return phrase
 }
