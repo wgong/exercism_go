37ec12dc0c0fe9e56414c613d563433935c5275b
WGong <wen.g.gong@gmail.com>
2018-12-23 10:23:51 -0500

    optimize scrabble-score

--- scrabble-score/scrabble-score.go
+++ scrabble-score/scrabble-score.go
@@ -1,33 +1,46 @@
 // Package scrabble calculates score
 package scrabble
 
-import "strings"
+import (
+	"strings"
+)
+
+var mScore = map[string]int{
+	"A, E, I, O, U, L, N, R, S, T": 1,
+	"D, G":          2,
+	"B, C, M, P":    3,
+	"F, H, V, W, Y": 4,
+	"K":             5,
+	"J, X":          8,
+	"Q, Z":          10}
+
+var bMapDefined = false
+
+func defineMap() {
+	for k, v := range mScore {
+		for _, i := range strings.Split(k, ",") {
+			mScore[strings.TrimSpace(i)] = v
+		}
+	}
+	// remove initial entry
+	for k := range mScore {
+		if len(k) > 1 {
+			delete(mScore, k)
+		}
+	}
+	bMapDefined = true
+}
 
 // Score calculates scrabble score
 func Score(word string) int {
-	// initial lookup
-	mStringScore := map[string]int{
-		"A, E, I, O, U, L, N, R, S, T": 1,
-		"D, G":          2,
-		"B, C, M, P":    3,
-		"F, H, V, W, Y": 4,
-		"K":             5,
-		"J, X":          8,
-		"Q, Z":          10}
-
-	// lookup per letter
-	mLetterScore := map[string]int{}
-	for k, v := range mStringScore {
-		//fmt.Println(k,v)
-		for _, i := range strings.Split(k, ",") {
-			mLetterScore[strings.TrimSpace(i)] = v
-		}
+	if bMapDefined != true {
+		defineMap()
 	}
 
 	score := 0
 	word = strings.ToUpper(word)
 	for _, c := range word {
-		score += mLetterScore[string(c)]
+		score += mScore[string(c)]
 	}
 	return score
 }
