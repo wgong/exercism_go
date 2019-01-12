fe43a837e4429ade660f6514f3b995c834f41022
WGONG <wen.g.gong@gmail.com>
2019-01-11 22:46:35 -0500

    robot_name bonus

--- exercism/robot-name/robot_name.go
+++ exercism/robot-name/robot_name.go
@@ -11,24 +11,39 @@ type Robot struct {
 	name string
 }
 
+//ensure uniqueness
+var mapNames map[string]bool
+
 func init() {
 	rand.Seed(time.Now().UnixNano())
+	mapNames = map[string]bool{}
 }
 
 func genName() string {
 	return string(rand.Intn(26)+'A') + string(rand.Intn(26)+'A') + fmt.Sprintf("%03d", rand.Intn(1000))
 }
 
+func genUniqName() string {
+	for {
+		name := genName()
+		if mapNames[name] {
+			continue
+		}
+		mapNames[name] = true
+		return name
+	}
+}
+
 // Name names a robot
 func (r *Robot) Name() (string, error) {
 	if r.name == "" {
-		r.name = genName()
+		r.name = genUniqName() //genName()
 	}
 	return r.name, nil
 }
 
 // Reset re-names a robot
 func (r *Robot) Reset() (string, error) {
-	r.name = genName()
+	r.name = genUniqName() //genName()
 	return r.name, nil
 }
