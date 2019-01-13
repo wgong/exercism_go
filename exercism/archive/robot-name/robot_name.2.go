a9b60b13d52ae4462d71807f04bf4aaf741435de
WGONG <wen.g.gong@gmail.com>
2019-01-12 19:42:47 -0500

    extra

---
+++ exercism/archive/robot-name/robot_name.2.go
@@ -0,0 +1,49 @@
+package robotname
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Robot is type for robot
+type Robot struct {
+	name string
+}
+
+//ensure uniqueness
+var mapNames map[string]bool
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+	mapNames = map[string]bool{}
+}
+
+func genName() string {
+	return string(rand.Intn(26)+'A') + string(rand.Intn(26)+'A') + fmt.Sprintf("%03d", rand.Intn(1000))
+}
+
+func genUniqName() string {
+	for {
+		name := genName()
+		if mapNames[name] {
+			continue
+		}
+		mapNames[name] = true
+		return name
+	}
+}
+
+// Name names a robot
+func (r *Robot) Name() (string, error) {
+	if r.name == "" {
+		r.name = genUniqName() //genName()
+	}
+	return r.name, nil
+}
+
+// Reset re-names a robot
+func (r *Robot) Reset() (string, error) {
+	r.name = genUniqName() //genName()
+	return r.name, nil
+}
